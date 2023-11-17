4ca4ca42e0443502c8c9ea994901be887bc38ca7
zjxWeb <1184159464@qq.com>
2023-11-17 10:06:56 +0800

    73

---
+++ 73.cpp
@@ -0,0 +1,72 @@
+/**
+ * @file 73.cpp
+ * @author zjxweb
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-17
+ * 
+ * @copyright Copyright (c) 2023
+ *  矩阵置零
+ * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ * 
+ * 示例 1：
+输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+输出：[[1,0,1],[0,0,0],[1,0,1]]
+
+示例 2：
+输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+提示：
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+ 
+
+进阶：
+
+一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+你能想出一个仅使用常量空间的解决方案吗？
+
+ */
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
+        //将当前元素为0的所在行和列所有元素置为0
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main(){
+    vector<vector<int>> matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+    Solution s;
+    s.setZeroes(matrix);
+    return 0;
+}
+
