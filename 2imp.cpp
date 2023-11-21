308f46c941abdc8560ea2a8c7309d2bc2062ecda
zjxWeb <1184159464@qq.com>
2023-11-21 09:56:22 +0800

    2

--- 2imp.cpp
+++ 2imp.cpp
@@ -22,10 +22,29 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == nullptr && l2 == nullptr) return nullptr;
-        if(l1 == nullptr) return l2;
-        if(l2 == nullptr) return l1;
-
+        // 两数对应的值为n1 n2  进位值为 carry
+        // sum = n1 + n2 + carry
+        // 对应位置的值为sum % 10
+        // 新的进位制为 sum / 10
+        ListNode* head = nullptr,*tail = nullptr;
+        int carry = 0;
+        while(l1!= nullptr || l2!= nullptr){
+            int n1 = l1 == nullptr? 0 : l1->val;
+            int n2 = l2 == nullptr? 0 : l2->val;
+            int sum = n1 + n2 + carry;
+            if(!head){
+                head = tail = new ListNode(sum % 10);
+            }
+            else{
+                tail->next = new ListNode(sum % 10);
+                tail = tail->next;
+            }
+            carry = sum / 10;
+            if(l1)l1 = l1->next;
+            if(l2) l2 = l2->next;
+        }
+        if(carry > 0) tail->next = new ListNode(carry);
+        return head;
     }
 };
 
