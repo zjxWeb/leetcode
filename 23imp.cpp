be1ffa860bdc5afddfcadd786b89352546ad3179
zjxWeb <1184159464@qq.com>
2023-11-24 16:05:36 +0800

    priority_queue

---
+++ 23imp.cpp
@@ -0,0 +1,99 @@
+/**
+ * @file 23.cpp 合并 K 个升序链表
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-24
+ * @cite  https://leetcode.cn/problems/merge-k-sorted-lists/?envType=study-plan-v2&envId=top-100-liked
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         int n = lists.size();
+//         ListNode* res = nullptr;
+//         if(n == 0)return nullptr;
+//         if(n == 1)return lists[0];
+//         for(int i = 0;i < n - 1;i++) res = merge(res,lists[i]);
+//         return res;
+//     }
+//     ListNode* merge(ListNode* l1, ListNode* l2){
+//         ListNode* dummy = new ListNode(0);
+//         ListNode * temp = dummy;
+//         ListNode* temp1 = l1,*temp2 = l2;
+//         if ((!l1) || (!l2)) return l1 ? l1 : l2;
+//         while(temp1 != nullptr && temp2 != nullptr){
+//             if(temp1->val < temp2->val){
+//                 temp->next = temp1;
+//                 temp1 = temp1->next;
+//             }
+//             else{
+//                 temp->next = temp2;
+//                 temp2 = temp2->next;
+//             }
+//             temp = temp->next;
+//         }
+//         temp->next = (temp1 ? temp1 : temp2);
+//         return dummy->next;
+//     }
+// };
+
+// 优先队列
+class Solution {
+public:
+    struct Status {
+        int val;
+        ListNode *ptr;
+        bool operator < (const Status &rhs) const {
+            return val > rhs.val;
+        }
+    };
+
+    priority_queue <Status> q;
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        for (auto node: lists) {
+            if (node) q.push({node->val, node});
+        }
+        ListNode head, *tail = &head;
+        while (!q.empty()) {
+            auto f = q.top(); q.pop();
+            tail->next = f.ptr; 
+            tail = tail->next;
+            if (f.ptr->next) q.push({f.ptr->next->val, f.ptr->next});
+        }
+        return head.next;
+    }
+};
+
+
+int main(){
+    Solution s;
+    vector<ListNode*> lists = {
+        new ListNode(1,new ListNode(4,new ListNode(5))),
+        new ListNode(1,new ListNode(3,new ListNode(4))),
+        new ListNode(2,new ListNode(6))
+    };
+    ListNode* ans = s.mergeKLists(lists);
+    while(ans){
+        cout << ans->val << endl;
+        ans = ans->next;
+    }
+    return 0;
+}
+
