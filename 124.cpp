85c7e5020e244a19c4b494408bdc20a53d34bc7f
zjxWeb <1184159464@qq.com>
2023-11-30 10:08:35 +0800

    124.cpp 二叉树中的最大路径和

--- 124.cpp
+++ 124.cpp
@@ -25,7 +25,20 @@ struct TreeNode {
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        
+        maxSum(root);
+        return maxVal;
+    }
+private:
+    int maxVal = INT_MIN;
+    int maxSum(TreeNode* root){
+        if(root == nullptr) return 0;
+        int left = max(maxSum(root->left), 0);
+        int right = max(maxSum(root->right), 0);
+        // 左右子树的最大路径和
+        int sum = root->val + left + right;
+        // 左右子树的最大路径和 + 当前节点的路径和
+        maxVal = max(maxVal, sum);
+        return root->val + max(left,right);
     }
 };
 
