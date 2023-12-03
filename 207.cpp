0a32955ee64cf0aa23569c94655aeb4994d1cdb9
zjxweb <1184159464@qq.com>
2023-12-03 16:12:36 +0800

    1

---
+++ 207.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file 207.cpp 课程表
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-03
+ * @cite https://leetcode.cn/problems/course-schedule/?envType=study-plan-v2&envId=top-100-liked
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> prerequisites = {{1,0},{0,1}};
+    cout << s.canFinish(2, prerequisites) << endl;
+    return 0;
+}
