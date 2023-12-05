c1bdb6bbac95e34681c6faf70dcb37d5dfc2bb30
zjxWeb <1184159464@qq.com>
2023-12-05 17:05:52 +0800

    22.cpp 括号生成

---
+++ 22.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file 22.cpp 括号生成
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-05
+ * @cite https://leetcode.cn/problems/generate-parentheses/description/?envType=study-plan-v2&envId=top-100-liked
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream> 
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+    }
+};
+int main(){
+    Solution s;
+    vector<string> res = s.generateParenthesis(3);
+    for(auto &e : res){
+        cout << e << endl;
+    }
+    return 0;
+
+}
