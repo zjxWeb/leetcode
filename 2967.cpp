7273021a64689c6d67e0d8292800a42a42db10b9
zjxweb <1184159464@qq.com>
2023-12-18 17:30:34 +0800

    2967.cpp  使数组成为等数数组的最小代价

---
+++ 2967.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file 2967.cpp  使数组成为等数数组的最小代价
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-18
+ * @cite https://leetcode.cn/problems/minimum-cost-to-make-array-equalindromic/description/
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    long long minimumCost(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums = {10,12,13,14,15};
+    cout << s.minimumCost(nums) << endl;
+    return 0;
+}
