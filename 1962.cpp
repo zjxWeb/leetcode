d0bed14f89e409213037af554b715c64c4c51833
zjxWeb <1184159464@qq.com>
2023-12-23 16:08:22 +0800

    1

---
+++ 1962.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file 1962.cpp  移除石子使总数最小   
+ * @author your name (you@domain.com)
+ * @brief  https://leetcode.cn/problems/remove-stones-to-minimize-the-total/description/
+ * @version 0.1
+ * @date 2023-12-23
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+
+    }
+};
+int main(){
+    Solution s;
+    vector<int> piles = {5, 3, 1, 4, 2};
+    int k = 2;
+    cout << s.minStoneSum(piles, k) << endl;
+    return 0;
+}
